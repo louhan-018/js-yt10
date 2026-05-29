4ef9e0afc50be12bad30d050b204b55a4c1f551c
Louhan Rout <louhanrout9876@gmail.com>
2026-05-29 05:56:55 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 124067
+let accountEmail = "louhan@google.com"
+var accountPwd = "2005"
+accountCity = "Balasore"
+let accountState;
+
+// accountId = 7   // in 'const' changes are not allowed
+accountEmail = "omm@gmail.com"
+accountPwd = "0506"
+accountCity = "bbsr"
+
+/*
+    prefer not to use 'var'
+    bcz of issue in block scope and functioal scope
+*/
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPwd, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict";  // treat all JS Code as newer version
+
+// alert(6 + 9)  // we are using nodejs , not browser
+
+console.log(6 + 9); console.log("Louhan")  // bad practices reduce code readability
+
+let name = "Louhan"
+let age = 21
+let isActive = true
+let temp = null
+let state;
+
+// DataTypes
+/*
+
+1. Number (2^53)
+2. bigint
+3. string ("")
+4. boolean (true/false)
+5. null (standalone value)
+6. undefined
+7. symbol (uniqueness)
+8. object
+
+*/
+
+console.log(typeof "Louhan");
+console.log(typeof null);   // o/p = object(imp)
+console.log(typeof undefined);
+console.log(typeof true);
+
