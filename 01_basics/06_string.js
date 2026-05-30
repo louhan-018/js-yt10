4d12f3f9a2bb4c9c385e2b0d9c52843bb0c2eff9
Louhan Rout <louhanrout9876@gmail.com>
2026-05-30 15:24:23 +0000

    string done

---
+++ 01_basics/06_string.js
@@ -0,0 +1,34 @@
+const name = "Louhan Rout"
+const repo_cnt = 10
+
+// console.log(name + repo_cnt + " qtty");
+
+console.log(`my name is ${name} and my repo count is ${repo_cnt}.`);
+
+const gameName = new String('Louhan')
+
+console.log(typeof gameName);
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne = "   hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('sundar'))
+
+// console.log(gameName.split('-'));
