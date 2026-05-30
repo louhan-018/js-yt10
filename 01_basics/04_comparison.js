0dac4404c392a7e648a109f4df01da16957eb445
Louhan Rout <louhanrout9876@gmail.com>
2026-05-30 04:19:20 +0000

    imp comparisons

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+// console.log(3 >6);
+// console.log(3 >= 6);
+// console.log(3 <= 6);
+// console.log(3 == 6);
+
+// console.log("3" > 1);
+// console.log("03" > 1);
+
+// console.log(null == 0);  
+// console.log(null >= 0);  // comparison case the 'null' is converted to number
+// console.log(null <= 0);
+// console.log(null > 0);
+// console.log(null < 0);
+
+// console.log(undefined == 0);
+// console.log(undefined >= 0);  // same as in 'undefined'
+// console.log(undefined <= 0);
+// console.log(undefined < 0);
+// console.log(undefined > 0);
+
+// in best practice we avoid these 'null' && 'undefined' type comparison
+
+// strict check (===)
+
+console.log("3" === 3);
