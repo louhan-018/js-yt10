433e31a5c9ed6a0e19428b7fa7301f862334c76f
Louhan Rout <louhanrout9876@gmail.com>
2026-05-30 04:57:18 +0000

    data_typeSummary

---
+++ 01_basics/05_dataTypes_summary.js
@@ -0,0 +1,35 @@
+// ******************PRIMITIVE******************
+// 7 types: string, number, boolean, null, undefined, symbol, BigInt
+// (call by value)
+
+const score = 100
+const valueScore = 100.5
+
+const isActive = false
+const userEmail = null
+let state;
+
+const id = Symbol('789')
+const subId = Symbol('123')
+// console.log(id === subId);
+
+const bigNum = 423721539161233896n
+// console.log(typeof bigNum);
+
+
+
+// **********NON-PRIMITIVE(reference)********* 
+// Array, Objects, Functions
+
+const heros = ["superman", "batman", "ironman"]
+
+let myObject = {
+    name: "Louhan",
+    age: 21,
+}
+
+const myFunction = function(){
+        console.log("Hello");   
+}
+
+console.log(typeof heros);
